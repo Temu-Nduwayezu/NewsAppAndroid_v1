aff45708f1bd31d86f309f69e1ec65178f193051
GLO-MK <91663344+Temu-Nduwayezu@users.noreply.github.com>
2023-05-27 21:00:44 +0200

    Delete NewsApp_v1-master directory
    duplicate delete

--- NewsApp_v1-master/app/src/main/java/at/ac/fhcampuswien/newsapp/data/models/NewsResponse.kt
+++
@@ -1,5 +0,0 @@
-package at.ac.fhcampuswien.newsapp.data.models
-
-data class NewsResponse(val status : String? = null,
-                        val totalResults : Int? = null,
-                        val articles : List<Article>? = null)
--- NewsApp_v1-master/app/src/main/java/at/ac/fhcampuswien/newsapp/data/models/Source.kt
+++
@@ -1,4 +0,0 @@
-package at.ac.fhcampuswien.newsapp.data.models
-
-data class Source(val id : String? = null,
-                  val name : String? = null)
